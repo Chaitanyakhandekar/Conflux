2d9d02fca9402742d47218cf70c2dc4eb42a8fd4
Chaitanya <chaitanyakhandekar95@gmail.com>
2026-07-10 18:21:00 +0530

    connected controller,service ,roue for google auth

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { registerUser, loginUser, resendOTPEmail, verifyOTP, authMe } from "../controllers/auth.controller.ts";
+import { registerUser, loginUser, resendOTPEmail, verifyOTP, authMe,continueWithGoogleController } from "../controllers/auth.controller.ts";
 
 const router = Router()
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+router.route("/google").post(continueWithGoogleController)
 router.route("/resend-otp").post(resendOTPEmail)
 router.route("/verify-otp").post(verifyOTP)
 router.route("/auth-me").get(authMe)
 
-export default router;
+export default router;  
